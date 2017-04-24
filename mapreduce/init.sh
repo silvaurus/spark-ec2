9c5c9e807eacf594e516ae4a7679dcbe19910704
Henry Yi He <yiizy@uchicago.edu>
2017-04-23 21:48:25 -0500

    v1.4

--- mapreduce/init.sh
+++ mapreduce/init.sh
@@ -5,11 +5,11 @@ case "$HADOOP_MAJOR_VERSION" in
   1)
     echo "Nothing to initialize for MapReduce in Hadoop 1"
     ;;
-  2) 
-    wget http://s3.amazonaws.com/spark-related-packages/mr1-2.0.0-mr1-cdh4.2.0.tar.gz 
+  2)
+    wget http://s3.amazonaws.com/spark-related-packages/mr1-2.0.0-mr1-cdh4.2.0.tar.gz
     tar -xvzf mr1-*.tar.gz > /tmp/spark-ec2_mapreduce.log
     rm mr1-*.tar.gz
-    mv hadoop-2.0.0-mr1-cdh4.2.0/ mapreduce/
+    mv hadoop-2.7.0/ mapreduce/
     ;;
   yarn)
     echo "Nothing to initialize for MapReduce in Hadoop 2 YARN"
